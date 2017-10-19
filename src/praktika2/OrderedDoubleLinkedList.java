0c9fffc0ab80b52d3dbc506bf970572fdcfe216e
Goizane <goizanealonso95@gmail.com>
2017-10-19 15:34:09 +0200

    Praktikaa 2

--- src/praktika2/OrderedDoubleLinkedList.java
+++ src/praktika2/OrderedDoubleLinkedList.java
@@ -4,9 +4,11 @@ public class OrderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements O
 	
 	public void add(T elem){
 	// Elementua gehitu dagokion posizioan zerrenda ordenatu batean
+	// Aurrebaldintza: Zerrenda txikitik handira dago ordenatuta.
 		
 		// KODEA OSATU ETA KOSTUA KALKULATU
-
-
+		
+		
+		// KOSTUA
 	}
 }
